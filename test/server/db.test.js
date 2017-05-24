c79f926cb2a24fbd1edb429c8b7cf37b1338fe1c
Thomasin Abraham <thomasinabraham@me.com>
2017-05-24 17:18:11 +1200

    5 tests passing

--- test/server/db.test.js
+++ test/server/db.test.js
@@ -5,12 +5,22 @@ configureDatabase(test)
 
 var db = require('../../server/db')
 
-test('getAllData returns array with at least id, date and mood', function (t) {
+test('getAllData returns array with at least id, date and mood', t => {
   return db.getAllData(t.context.connection)
     .then(function(result) {
       t.not(result.data[0].date, undefined)
       t.not(result.data[0].id, undefined)
       t.not(result.data[0].outlook, undefined)
       t.not(result.data[0].energy, undefined)
+    })
+})
+
+test('getAll returns a different array depending on tableName', t => {
+  return db.getAll(t.context.connection, 'entry')
+    .then((entryResults) => {
+      return db.getAll(t.context.connection, 'variable')
+        .then((variableResults) => {
+          t.not(entryResults[0].id, variableResults[0].id)
+        })
     })
 })
